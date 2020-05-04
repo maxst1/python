ff649cdaafeeff08680c72d44e1fbce54aebc7ee
maxst1 <stalev.ma@phystech.edu>
2020-05-05 02:58:48 +0300

    changes

--- dev/encrypter.py
+++ dev/encrypter.py
@@ -1,49 +1,28 @@
 import argparse
 import sys
-import json
-import math
+import pickle
 
-parser = argparse.ArgumentParser()
-parser.add_argument('encrypt_type', type=str)
-parser.add_argument('--chipher', type=str)
-parser.add_argument('--key', type=str)
-parser.add_argument('--input-file', type=str)
-parser.add_argument('--output-file', type=str)
-parser.add_argument('--text-file', type=str)
-parser.add_argument('--model-file', type=str)
-args = parser.parse_args()
 
-# алфавиты
-alphs = []
-alphs.append(' .,;:-!?\"\'')
-alphs.append('abcdefghijklmnopqrstuvwxyz')
-alphs.append('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-alphs.append('abcdefghijklmnopqrstuvwxyz'.upper())
-alphs.append('абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.upper())
 s = ''
-for alph in alphs:
-    s = s + alph
+s += ' .,;:-!?\"\''
+s += 'abcdefghijklmnopqrstuvwxyz'
+s += 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+s += 'abcdefghijklmnopqrstuvwxyz'.upper()
+s += 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.upper()
 
-if not args.encrypt_type in ['encode', 'decode', 'train', 'hack']:
-    print('Неизвестная команда, используйте encode, decode, train, hack')
 
-if args.input_file == '':
-    print('Не задано имя входного файла')
-    exit()
-
-if args.output_file == '':
-    print('Не задано имя выходного файла')
-    exit()
-
-# encode/decode
-if (args.encrypt_type == 'encode' or args.encrypt_type == 'decode'):
-
+def endecode(args):
     if (args.key is None) or (args.key == ''):
         print('\nНеправильный ключ')
         exit()
 
+    if args.chiper not in ['caesar', 'vigenere', 'vernam']:
+        print("\nТакого шифра не предусмотрено", end='')
+        print("(используйте caesar, vigenere, vernam)")
+        exit()
+
     # входная строка
-    if (args.input_file is None):
+    if (args.input_file is None or args.input_file == ""):
         try:
             input_text = sys.stdin.read()
         except KeyboardInterrupt:
@@ -52,92 +31,87 @@ if (args.encrypt_type == 'encode' or args.encrypt_type == 'decode'):
     else:
         input_text = ''
         try:
-            f = open(args.input_file)
+            with open(args.input_file) as f:
+                for line in f:
+                    input_text += line
         except FileNotFoundError:
             print('\nНет такого файла')
             exit()
 
-        for line in f:
-            input_text = input_text + line
-        f.close()
-
     k = 2 * int(args.encrypt_type == 'encode') - 1
-    """ если шифруем сдвигаем
-    вперед (k == +1), если дешифруем сдвигаем назад (k == -1)"""
+    # если шифруем сдвигаем
+    # вперед (k == +1), если дешифруем сдвигаем назад (k == -1)
 
     # выходная строка
     output_text = ''
 
     c = -1
-    b = False
     # шифрация/дешифрация
     for letter in input_text:
 
         char = letter
 
         if not (letter in s):
-            output_text = output_text + char
+            output_text += char
             continue
 
-        if (args.chipher == 'caesar'):
+        if (args.chiper == 'caesar'):
             try:
                 char = s[(s.index(letter) + k * int(args.key)) % len(s)]
             except ValueError:
                 print('\nКлюч в шифре Цезаря должен быть числом')
                 exit()
-
-        elif (args.chipher == 'vigenere'):
+        elif (args.chiper == 'vigenere'):
             c = (c + 1) % len(args.key)
             try:
                 char = s[(s.index(letter) + k * s.index(args.key[c])) % len(s)]
             except ValueError:
-                print("\nКодирование таким ключом не предусмотрено программой, используйте символы:\n\'" + alphs[0] + '\', а также буквы русского и латинского алфавитов')
+                print("\nКодирование таким ключом не", end='')
+                print("предусмотрено программой,", end='')
+                print("используйте символы:\n\'" + s)
+                exit()
+        elif (args.chiper == 'vernam'):
+            c = (c + 1) % len(args.key)
+            try:
+                char = s[s.index(letter) ^ s.index(args.key[c])]
+            except ValueError:
+                print("\nКодирование таким ключом не", end='')
+                print("предусмотрено программой,", end='')
+                print("используйте символы:\n" + s)
                 exit()
 
-        elif (args.chipher == 'vernam'):
-            c = (c + 1) % len(args.key)
-            char = s[s.index(letter) ^ s.index(args.key[c])]
-
-        else:
-            print("\nТакого шифра не предусмотрено(используйте caesar, vigenere, vernam)")
-            exit()
-
-        output_text = output_text + char
+        output_text += char
 
     # вывод
-    if (args.output_file is None):
+    if (args.output_file is None or args.output_file == ""):
         sys.stdout.write(output_text)
     else:
-        f = open(args.output_file, 'w')
-        f.write(output_text)
-        f.close()
+        with open(args.output_file, 'w') as f:
+            f.write(output_text)
     exit()
 
 
-if (args.model_file == '') or (args.model_file is None):
-    print('Не задано имя файла модели')
-    exit()
-
-# train
-if (args.encrypt_type == 'train'):
+def train(args):
+    if (args.model_file == '') or (args.model_file is None):
+        print('Не задано имя файла модели')
+        exit()
 
     #  входная строка
-    if (args.text_file is None):
+    if (args.text_file is None or args.text_file == ""):
         try:
             input_text = sys.stdin.read()
         except KeyboardInterrupt:
             print('\nОстановка пользователем')
             exit()
     else:
+        input_text = ''
         try:
-            f = open(args.text_file)
+            with open(args.text_file) as f:
+                for line in f:
+                    input_text += line
         except FileNotFoundError:
             print('\nНет такого файла')
             exit()
-        input_text = ''
-        for line in f:
-            input_text = input_text + line
-        f.close()
 
     #  словарь модели
     model_dict = {}  # type: dict
@@ -145,9 +119,8 @@ if (args.encrypt_type == 'train'):
     #  частоты букв, которых мы не умеем (де)кодировать не повлияют на
     #  нахождение "правильного" текста, поэтому в словарь добавляем
     # только те, что знаем
-    for alph in alphs:
-        for letter in alph:
-            model_dict.setdefault(letter, 0)
+    for letter in s:
+        model_dict.setdefault(letter, 0)
 
     count = 0
     for letter in input_text:
@@ -159,16 +132,14 @@ if (args.encrypt_type == 'train'):
         for key in model_dict.keys():
             model_dict[key] = float(model_dict[key] / count)
 
-    f = open(args.model_file + '.json', 'w')
-    json.dump(model_dict, f)
-    f.close()
+    with open(args.model_file + '.pickle', 'wb') as output_file:
+        pickle.dump(model_dict, output_file)
     exit()
 
-#  hack
-if (args.encrypt_type == 'hack'):
 
+def hack(args):
     #  индекс совпадения строки
-    def I(string, alph):
+    def findI(string, alph):
         freq = {}  # type: dict
         for letter in alph:
             freq.setdefault(letter, 0)
@@ -182,7 +153,6 @@ if (args.encrypt_type == 'hack'):
             summ = summ + freq[letter]*(freq[letter] - 1)
         summ = float(summ / (len(string) * (len(string) - 1)))
         return summ
-
 
     #  взаимный индекс совпадения строк
     def MI(string1, string2, alph):
@@ -207,7 +177,7 @@ if (args.encrypt_type == 'hack'):
         return summ
 
     #  входная строка
-    if (args.input_file is None):
+    if (args.input_file is None or args.input_file == ""):
         try:
             input_text = sys.stdin.read()
         except KeyboardInterrupt:
@@ -218,7 +188,7 @@ if (args.encrypt_type == 'hack'):
         try:
             with open(args.input_file) as f:
                 for line in f:
-                    input_text = input_text + line
+                    input_text += line
         except FileNotFoundError:
             print('\nНет такого файла')
             exit()
@@ -237,14 +207,14 @@ if (args.encrypt_type == 'hack'):
         for j in range(0, t):
             line = ''
             for i in range(j, len(mit), t):
-                line = line + mit[i]
+                line += mit[i]
             sl.append(line)
 
-        meanI = 0
+        meanI = float(0)
         for line in sl:
-            meanI = meanI + I(line, s)
+            meanI = meanI + findI(line, s)
 
-        meanI = float (meanI / t)
+        meanI = float(meanI / t)
         if meanI > 0.05:
             key_len = t
             break
@@ -253,17 +223,16 @@ if (args.encrypt_type == 'hack'):
     for j in range(0, key_len):
         line = ''
         for i in range(j, len(mit), key_len):
-            line = line + mit[i]
+            line += mit[i]
         sl.append(line)
 
-    print(key_len)
     for i in range(1, len(sl)):
         mmi = 0
         msd = 0
         for sd in range(len(s)):
             sd_s = ''
             for letter in sl[i]:
-                sd_s = sd_s + s[(s.index(letter) - sd) % len(s)]
+                sd_s += s[(s.index(letter) - sd) % len(s)]
 
             if MI(sl[0], sd_s, s) > mmi:
                 mmi = MI(sl[0], sd_s, s)
@@ -271,7 +240,7 @@ if (args.encrypt_type == 'hack'):
 
         sd_s = ''
         for letter in sl[i]:
-            sd_s = sd_s + s[(s.index(letter) - msd) % len(s)]
+            sd_s += s[(s.index(letter) - msd) % len(s)]
         sl[i] = sd_s
 
     mit = ''
@@ -279,15 +248,15 @@ if (args.encrypt_type == 'hack'):
         for j in range(len(sl)):
             if i >= len(sl[j]):
                 continue
-            mit = mit + sl[j][i]
+            mit += sl[j][i]
 
     input_textv = ''
     count = 0
     for letter in input_text:
-        if not letter in s:
-            input_textv = input_textv + letter
+        if letter not in s:
+            input_textv += letter
         else:
-            input_textv = input_textv + mit[count]
+            input_textv += mit[count]
             count = count + 1
 
     #  выходная строка
@@ -296,12 +265,9 @@ if (args.encrypt_type == 'hack'):
     #  словарь с текущими значениями частот
     current_freq = {}  # type: dict
 
-    #  cловарь результатов
-    res_dict = {}  # type: dict
-
     #  словарь "правильных" частот
-    with open(args.model_file + '.json') as f:
-        model_dict = json.load(f)
+    with open(args.model_file + '.pickle', 'rb') as input_file:
+        model_dict = pickle.load(input_file)
 
     for key in model_dict:
         model_dict[key] = float(model_dict[key])
@@ -319,7 +285,7 @@ if (args.encrypt_type == 'hack'):
         for key in current_freq.keys():
             current_freq[key] = float(current_freq[key] / count)
 
-    current_freqv = {}
+    current_freqv = {}  # type: dict
     for letter in s:
         current_freqv.setdefault(letter, 0)
 
@@ -333,7 +299,7 @@ if (args.encrypt_type == 'hack'):
         for key in current_freqv.keys():
             current_freqv[key] = float(current_freqv[key] / count)
 
-    m = len(s) + 1
+    m = float(len(s) + 1)
     key = 0
     b = True
     for j in range(len(s)):
@@ -360,8 +326,8 @@ if (args.encrypt_type == 'hack'):
                 value = current_freq[k]
                 valuev = current_freqv[k]
             i = i + 1
-            current_freq[k] = current_freq[s[(s.index(k) + 1) % len(s)]]
-            current_freqv[k] = current_freqv[s[(s.index(k) + 1) % len(s)]]
+            current_freq[k] = current_freq[s[(s.index(str(k)) + 1) % len(s)]]
+            current_freqv[k] = current_freqv[s[(s.index(str(k)) + 1) % len(s)]]
             if (i == len(current_freq)):
                 current_freq[k] = value
                 current_freqv[k] = valuev
@@ -378,13 +344,52 @@ if (args.encrypt_type == 'hack'):
         if letter in s:
             char = s[(s.index(letter) - key) % len(s)]
 
-        output_text = output_text + char
+        output_text += char
 
     #  вывод
-    if (args.output_file is None):
+    if (args.output_file is None or args.output_file == ""):
         sys.stdout.write(output_text)
     else:
-        f = open(args.output_file, 'w')
-        f.write(output_text)
-        f.close()
-    exit()
+        with open(args.output_file, 'w') as f:
+            f.write(output_text)
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='encrypt_type')
+
+# encode
+encode_parser = subparsers.add_parser('encode')
+encode_parser.add_argument('--chiper', type=str)
+encode_parser.add_argument('--key', type=str)
+encode_parser.add_argument('--input-file', type=str)
+encode_parser.add_argument('--output-file', type=str)
+encode_parser.set_defaults(func=endecode)
+
+# decode
+decode_parser = subparsers.add_parser('decode')
+decode_parser.add_argument('--chiper', type=str)
+decode_parser.add_argument('--key', type=str)
+decode_parser.add_argument('--input-file', type=str)
+decode_parser.add_argument('--output-file', type=str)
+decode_parser.set_defaults(func=endecode)
+
+# train
+train_parser = subparsers.add_parser('train')
+train_parser.add_argument('--text-file', type=str)
+train_parser.add_argument('--model-file', type=str)
+train_parser.set_defaults(func=train)
+
+# hack
+hack_parser = subparsers.add_parser('hack')
+hack_parser.add_argument('--input-file', type=str)
+hack_parser.add_argument('--output-file', type=str)
+hack_parser.add_argument('--model-file', type=str)
+hack_parser.set_defaults(func=hack)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.encrypt_type not in ['encode', 'decode', 'train', 'hack']:
+        print("\nНеизвестная команда")
+        print("Используте [<encode>, <decode>, <train>, <hack>]")
+        exit()
+    args.func(args)
